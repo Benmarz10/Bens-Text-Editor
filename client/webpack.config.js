ce84d8fe094a8d225e45c53db0b68993a14ddf62
Benmarz10 <benjaminmarz10@gmail.com>
2023-03-20 18:27:03 -0500

    Fixed install button

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
